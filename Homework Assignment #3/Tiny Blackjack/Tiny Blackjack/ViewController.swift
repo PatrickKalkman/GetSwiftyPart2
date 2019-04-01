444976eacf72047a6a6fcf77ec41b2010c16af95
Patrick Kalkman <PKa@batenburgtechniek.onmicrosoft.com>
2019-04-01 16:40:20 +0200

    changing to keyframe

--- Homework Assignment #3/Tiny Blackjack/Tiny Blackjack/ViewController.swift
+++ Homework Assignment #3/Tiny Blackjack/Tiny Blackjack/ViewController.swift
@@ -73,52 +73,65 @@ class ViewController: UIViewController, BlackjackViewProtocol {
         let cardToImageNameMapper: CardToImageNameMapper = CardToImageNameMapper()
         let imageName: String = cardToImageNameMapper.map(gameEngine.getPlayerCard(playerIndex: 0, handIndex: 0, cardIndex: 0))
         let imageCardFaceUp: UIImage = UIImage(named: imageName)!
-        
         let imageCardFaceDown: UIImage = UIImage(named: "Card.Facedown")!
-        
+
         let player1CardImageView: UIImageView = UIImageView(image: imageCardFaceDown)
-        
         player1CardImageView.frame = CGRect(x: deckCard.frame.origin.x, y: deckCard.frame.origin.y, width: deckCard.frame.size.width, height: deckCard.frame.size.height)
-        
         view.addSubview(player1CardImageView)
+
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: .calculationModeCubic, animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.8) {
+                self.player1CardImageView.frame.origin.y = 500
+                self.player1CardImageView.frame.origin.x = 400
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                // transitionFlipFromLeft
+                self.player1Card2ImageView.image = imageCard2FaceUp
+            }
+
+        })
+
+
         
-        UIImageView.animate(withDuration: 1, delay: 0.1, options: .curveEaseInOut, animations: {
-            player1CardImageView.frame.origin.y = 500
-            player1CardImageView.frame.origin.x = 400
-        }, completion: { _ in
-            UIImageView.transition(with: player1CardImageView,
-                                   duration: 0.5,
-                                   options: .transitionFlipFromLeft,
-                                   animations: { player1CardImageView.image = imageCardFaceUp },
-                                   completion: {_ in
+        // UIImageView.animate(withDuration: 1, delay: 0.1, options: .curveEaseInOut, animations: {
+        //     player1CardImageView.frame.origin.y = 500
+        //     player1CardImageView.frame.origin.x = 400
+        // }, completion: { _ in
+        //     UIImageView.transition(with: player1CardImageView,
+        //                            duration: 0.5,
+        //                            options: .transitionFlipFromLeft,
+        //                            animations: { player1CardImageView.image = imageCardFaceUp },
+        //                            completion: {_ in
                                     
-                                    let imageNameCard2: String = cardToImageNameMapper.map(self.gameEngine.getPlayerCard(playerIndex: 0, handIndex: 0, cardIndex: 1))
-                                    let imageCard2FaceUp: UIImage = UIImage(named: imageNameCard2)!
+        //                             let imageNameCard2: String = cardToImageNameMapper.map(self.gameEngine.getPlayerCard(playerIndex: 0, handIndex: 0, cardIndex: 1))
+        //                             let imageCard2FaceUp: UIImage = UIImage(named: imageNameCard2)!
                                     
-                                    let player1Card2ImageView: UIImageView = UIImageView(image: imageCardFaceDown)
+        //                             let player1Card2ImageView: UIImageView = UIImageView(image: imageCardFaceDown)
                                     
-                                    player1Card2ImageView.frame = CGRect(x: self.deckCard.frame.origin.x, y: self.deckCard.frame.origin.y, width: self.deckCard.frame.size.width, height: self.deckCard.frame.size.height)
+        //                             player1Card2ImageView.frame = CGRect(x: self.deckCard.frame.origin.x, y: self.deckCard.frame.origin.y, width: self.deckCard.frame.size.width, height: self.deckCard.frame.size.height)
                                     
-                                    self.view.addSubview(player1Card2ImageView)
+        //                             self.view.addSubview(player1Card2ImageView)
                                     
-                                    UIImageView.animate(withDuration: 1, delay: 0.1, options: .curveEaseInOut, animations: {
-                                        player1Card2ImageView.frame.origin.y = 500
-                                        player1Card2ImageView.frame.origin.x = 450
-                                    }, completion: { _ in
-                                        UIImageView.transition(with: player1Card2ImageView,
-                                                               duration: 0.5,
-                                                               options: .transitionFlipFromLeft,
-                                                               animations: { player1Card2ImageView.image = imageCard2FaceUp },
-                                                               completion: nil)
-                                    }
-                                    )
-                                    
-                                    
+        //                             UIImageView.animate(withDuration: 1, delay: 0.1, options: .curveEaseInOut, animations: {
+        //                                 player1Card2ImageView.frame.origin.y = 500
+        //                                 player1Card2ImageView.frame.origin.x = 450
+        //                             }, completion: { _ in
+        //                                 UIImageView.transition(with: player1Card2ImageView,
+        //                                                        duration: 0.5,
+        //                                                        options: .transitionFlipFromLeft,
+        //                                                        animations: { player1Card2ImageView.image = imageCard2FaceUp },
+        //                                                        completion: nil)
+        //                             }
+        //                             )
                                     
                                     
-            })
-        }
-        )
+                                    
+                                    
+        //     })
+        // }
+        // )
         
 
         
