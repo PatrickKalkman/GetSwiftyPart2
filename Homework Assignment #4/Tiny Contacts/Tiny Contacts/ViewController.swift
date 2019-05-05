39798b09fe711744c235bd01089870db32672642
Patrick Kalkman <patrick@simpletechture.nl>
2019-05-05 09:19:11 +0200

    Created xCode project

---
+++ Homework Assignment #4/Tiny Contacts/Tiny Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiny Contacts
+//
+//  Created by Patrick Kalkman on 05/05/2019.
+//  Copyright © 2019 SimpleTechture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
